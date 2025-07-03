a13b56da0c8ca73a87fdc2eb88374201e5b7f026
Faster-knight <140014502+Faster-knight@users.noreply.github.com>
2025-07-03 18:58:00 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,20 @@
 import java.util.Random;
 
 public class Main {
+    // [RUSSIAN] Данное поле класса содержит размер вложенного массива
     public static final int SIZE = 8;
-    public static void printMatrix(int[][] matrixStart, int[][]  matrixResult) {
-        System.out.println("Начальная матрица");
-        for (int[] ints : matrixStart) {
-            for (int j = 0; j < ints.length; j++) {
-                System.out.format("%4d", ints[j]);
-            }
-            System.out.println();
-        }
-        System.out.println("Конечная матрица с поворотом");
-        for (int[] ints : matrixResult) {
+    // [RUSSIAN] Данный метод выводит входную матрицу на экран
+    public static void printMatrix(int[][] matrixPrint) {
+        for (int[] ints : matrixPrint) {
             for (int j = 0; j < ints.length; j++) {
                 System.out.format("%4d", ints[j]);
             }
             System.out.println();
         }
     }
+    // [RUSSIAN] Данный метод запускает программу и заполняет матрицу рандомными значениями
+    // После чего проиходит ее поворот на 90 градусов по часовой стрелке
+    // После произведения поворота происходит вывод результата
     public static void main(String[] args) {
         int[][] startColors = new int[SIZE][SIZE];
         int[][] rotatedColors = new int[SIZE][SIZE];
@@ -32,6 +29,9 @@ public class Main {
                 rotatedColors[i][j] = startColors[SIZE - 1 - j][i];
             }
         }
-        printMatrix(startColors, rotatedColors);
+        System.out.println("Начальная матрица");
+        printMatrix(startColors);
+        System.out.println("Конечная матрица с поворотом");
+        printMatrix(rotatedColors);
     }
 }
